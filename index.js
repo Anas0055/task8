c71016a7373950fb1356be9efb8aacdbbad3f2d8
Anas0055 <manaskhan969@gmail.com>
2024-01-23 02:18:40 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable.
+//  Then, using that variable, create a message that reveals your 
+//  favorite number. Print that message.
+var favoritenumber = 10;
+var message = ("my favorite mumberis:".concat(favoritenumber));
+console.log(message);
---
+++ index.ts
@@ -0,0 +1,8 @@
+
+ // Favorite Number: Store your favorite number in a variable.
+ //  Then, using that variable, create a message that reveals your 
+ //  favorite number. Print that message.
+ 
+let favoritenumber:number=10
+let message:string =(`my favorite mumberis:${favoritenumber}`);
+console.log(message);
